db3177e9f26c632b85c3914cfea4fc8f22f0d22e
Michel Tomas <michel@mygooder.com>
2017-11-11 20:54:27 +0100

    Remove extra debug left

--- MangoPay/Libraries/RestTool.php
+++ MangoPay/Libraries/RestTool.php
@@ -192,7 +192,6 @@ class RestTool
             if (in_array(self::$_JSON_HEADER, $httpHeaders)) {
                 // FIXME This can also fail hard and is not checked.
                 $this->_requestData = json_encode($this->_requestData);
-                $this->logger->debug('RequestData JSON :' . print_r($this->_requestData, true));
                 if ($this->_root->Config->DebugMode) {
                     $logClass::Debug('RequestData JSON', $this->_requestData);
                 }
